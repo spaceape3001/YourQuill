c0beb26fc60fb97c6a63946da7e94098605d99bb
spaceape3001 <spaceape3001@users.noreply.github.com>
2021-02-18 10:02:46 +0000

    Adding in the unit test too :)

---
+++ tests/attr/attr_cp.cpp
@@ -0,0 +1,26 @@
+#include "yq/io/StdFile.hpp"
+#include <iostream>
+#include <filesystem>
+
+int main(int argc, char* argv[])
+{
+    if(argc<3){
+        std::cerr << "Usage: " << argv[0] << " (file-in) (file-out)\n";
+        return -1;
+    }
+    
+    std::filesystem::path  pin(argv[1]), pout(argv[2]);
+    
+    StdBodyFile         file;
+    if(!file.load(pin)){
+        std::cerr << "Unable to load\n";
+        return -1;
+    }
+    
+    if(!file.save_to(pout)){
+        std::cerr << "Unable to save\n";
+        return -1;
+    }
+    
+    return 0;
+}
