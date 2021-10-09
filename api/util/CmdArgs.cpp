1c31ee9dfbd052fc28dfa8ddb7e86d263de1118b
spaceape3001 <spaceape3001@users.noreply.github.com>
2021-10-09 12:22:08 +0000

    Adding command line arguments

---
+++ api/util/CmdArgs.cpp
@@ -0,0 +1,49 @@
+#include "CmdArgs.hpp"
+#include "ThreadId.hpp"
+#include <span>
+
+struct CmdArgs::Repo {
+    CmdArgs*                app = nullptr;
+    std::span<const char*>  args;
+    std::string_view        exe;
+};
+
+CmdArgs::Repo& CmdArgs::repo()
+{
+    static Repo _repo;
+    return _repo;
+}
+
+
+CmdArgs::CmdArgs(int argc, char**argv)
+{
+    if(!thread_id()){
+        Repo& _r    = repo();
+        if(!_r.app){
+            _r.app      = this;
+            _r.args     = std::span<const char*>((const char**) argv, argc);
+            _r.exe      = argv[0];
+        }
+    }
+}
+
+CmdArgs::~CmdArgs()
+{
+    if(!thread_id()){
+        Repo& _r    = repo();
+        if(_r.app == this)
+            _r.app  = nullptr;
+    }
+}
+
+
+std::string_view         CmdArgs::appName()
+{
+    return repo().exe;
+}
+    
+std::span<const char*>   CmdArgs::commandLine()
+{
+    return repo().args;
+}
+
---
+++ api/util/CmdArgs.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <span>
+#include <string_view>
+
+class CmdArgs {
+public:
+    
+    CmdArgs(const CmdArgs&) = delete;
+    CmdArgs(CmdArgs&&) = delete;
+    CmdArgs& operator=(const CmdArgs&) = delete;
+    CmdArgs& operator=(CmdArgs&&) = delete;
+    
+    CmdArgs(int, char**);
+    ~CmdArgs();
+    
+    static std::string_view         appName();
+    static std::span<const char*>   commandLine();
+
+private:
+    struct Repo;
+    static Repo& repo();
+};
