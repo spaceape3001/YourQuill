30eb5626510424cd20644e91089c0f4394e064b7
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-05-07 10:48:31 +0000

    Consolidated the variables

--- plugins/stdquill/variables.cpp
+++ plugins/stdquill/variables.cpp
@@ -7,6 +7,8 @@
 #include "common.hpp"
 
 #include <yq/app/DelayInit.hpp>
+#include <yq/io/file_utils.hpp>
+#include <yq/net/Http.hpp>
 #include <yq/stream/Ops.hpp>
 #include <yq/web/WebAdapters.hpp>
 #include <yq/web/WebHtml.hpp>
@@ -16,23 +18,175 @@
 
 using namespace yq;
 
-#include "var/abbr.ipp"
-#include "var/author.ipp"
-#include "var/body.ipp"
-#include "var/can_edit.ipp"
-#include "var/footer.ipp"
-#include "var/home.ipp"
-#include "var/host.ipp"
-#include "var/local.ipp"
-#include "var/logged_in.ipp"
-#include "var/name.ipp"
-#include "var/port.ipp"
-#include "var/scripts.ipp"
-#include "var/ssid.ipp"
-#include "var/summary.ipp"
-#include "var/tabbar.ipp"
-#include "var/time.ipp"
-#include "var/title.ipp"
-#include "var/user.ipp"
-#include "var/year.ipp"
+namespace {
+    void    var_abbr(WebHtml&h)
+    {
+        html_escape_write(h, wksp::abbreviation());
+    }
+
+    void    var_author(WebHtml&h)
+    {
+        html_escape_write(h, wksp::author());
+    }
+
+    void    var_body(WebHtml& h)
+    {
+        h << h.context().var_body;
+    }
+
+    void    var_can_edit(WebHtml&h)
+    {
+        //  TODO
+        h << true;
+    }
+
+    void    var_footer(WebHtml&h)
+    {
+        Ref<Template>    footer  = gFooter;
+        if(footer)
+            footer -> execute(h);
+    }
+
+    void    var_home(WebHtml&h)
+    {
+        html_escape_write(h, wksp::home());
+    }
+
+
+    void    var_host(WebHtml&h)
+    {
+        html_escape_write(h, wksp::host());
+    }
+
+    void    var_local(WebHtml& h)
+    {
+        h << h.context().is_local();
+    }
+
+
+    void    var_logged_in(WebHtml& h)
+    {
+        //  TODO
+        h << true;
+    }
+
+    void    var_name(WebHtml& h)
+    {
+        html_escape_write(h, wksp::name());
+    }
+
+    void    var_port(WebHtml&h)
+    {
+        h << wksp::port();
+    }
+
+
+    void    add_script(WebHtml& str, std::string_view name)
+    {
+        switch(mimeTypeForExt(file_extension(name))){
+        case ContentType::javascript:
+            str << "\t\t<script srce=\"/js/" << name << "\">\n";
+            break;
+        default:
+            //  do nothing if unclear...
+            break;
+        }
+    }
+    
+    void    var_scripts(WebHtml& h)
+    {
+        WebContext& ctx = h.context();
+        for(std::string_view s : ctx.page->scripts())
+            add_script(h, s);
+        for(const std::string& s : ctx.var_scripts)
+            add_script(h, s);
+    }
+
+    YQ_INVOKE( reg_webvar<var_scripts>("scripts"); )
+
+    void    var_ssid(WebHtml& h)
+    {
+        h << h.context().session.ssid;
+    }
+
+    YQ_INVOKE( reg_webvar<var_ssid>("ssid"); )
+
+
+    void    var_summary(WebHtml&h)
+    {
+        Ref<Template>    summary  = gSummary;
+        if(summary)
+            summary -> execute(h);
+    }
+
+    void    var_tabbar(WebHtml& h)
+    {
+        WebContext& ctx = h.context();
+        const WebGroup* grp = ctx.page -> group();
+        if(!grp)
+            return ;
+    
+        h << "<table class=\"tabbar\"><tr>\n";
+        for(const WebPage* p : grp -> pages){
+            bool    us = p == ctx.page;
+            if(us) {
+                h << "<td class=\"tb-select\">";
+                html_escape_write(h, p -> label());
+                h << "</td>";
+            } else {
+                UrlView url = ctx.url;
+                url.path    = p->path();
+                h << "<td class=\"tabbar\"><a href=\"" << url << "\">";
+                html_escape_write(h, p -> label());
+                h << "</a></td>";
+            }
+        }
+        h << "</td></table>\n";
+    }
+
+    void    var_time(WebHtml& h)
+    {
+        h  << h.context().timestamp;
+    }
+    
+    void    var_title(WebHtml&h)
+    {
+        html_escape_write(h, h.context().var_title);
+    }
+
+
+    void    var_user(WebHtml& h)
+    {
+        auto& ctx = h.context().session;
+        if(!ctx.logged_in)
+            h << "(anonymous)";
+        else
+            h << ctx.username;
+    }
+
+    void    var_year(WebHtml& h)
+    {
+        h << (h.context().timeparts.tm_year+1900);
+    }
+
+    YQ_INVOKE( 
+        reg_webvar<var_abbr>("abbr");
+        reg_webvar<var_author>("author"); 
+        reg_webvar<var_body>("body");
+        reg_webvar<var_can_edit>("can_edit").todo();        
+        reg_webvar<var_footer>("footer");
+        reg_webvar<var_home>("home");        
+        reg_webvar<var_host>("host");
+        reg_webvar<var_local>("local");
+        reg_webvar<var_logged_in>("logged_in").todo(); 
+        reg_webvar<var_name>("name");
+        reg_webvar<var_port>("port");
+        reg_webvar<var_summary>("summary"); 
+        reg_webvar<var_tabbar>("tabbar");
+        reg_webvar<var_time>("time");
+        reg_webvar<var_title>("title");
+        reg_webvar<var_user>("user");
+        reg_webvar<var_year>("year"); 
+    )
+}
 
