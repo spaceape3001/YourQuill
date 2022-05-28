744bde4c207cec3af77ae289b2160fefb30663b7
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-05-28 18:08:21 +0000

    Enable ImGui
    ImGUI is now in a library, ready for use.

---
+++ 3rd/imgui.cmake
@@ -0,0 +1,20 @@
+add_library(imgui SHARED 
+    imgui/imgui.h
+    imgui/imgui.cpp
+    imgui/imgui_demo.cpp
+    imgui/imgui_draw.cpp
+    imgui/imgui_tables.cpp
+    imgui/imgui_widgets.cpp
+    imgui/backends/imgui_impl_vulkan.cpp
+    imgui/backends/imgui_impl_glfw.cpp
+    imgui/misc/cpp/imgui_stdlib.cpp
+    #imgui/misc/freetype/imgui_freetype.cpp
+)
+
+target_link_libraries(imgui PUBLIC 
+    ${Vulkan_LIBRARIES} ${GLFW3_LIBRARY}
+)
+    
+LinkTest(imgui)
+
+
