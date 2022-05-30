2f31656e4b14143967be5cea4563eeb9d78dde1a
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-05-30 06:06:32 +0000

    Bump minimum gcc version

--- cmake/CheckCxxVersions.cmake
+++ cmake/CheckCxxVersions.cmake
@@ -1,6 +1,6 @@
 
 if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
-    if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "11.0.0")
+    if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "12.0.0")
         message(FATAL_ERROR "GCC version is obsolete, upgrade.")
     endif()
     SET(GCC ON)
