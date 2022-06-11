3dde4faba85d12f9f6f6031490d82127a9373cef
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-06-11 22:40:22 +0000

    Adding vulkan mamory allocation library

---
+++ 3rd/vma.cmake
@@ -0,0 +1,11 @@
+add_library(vma SHARED 
+    vma.cpp
+)
+
+target_include_directories(vma PUBLIC ./vma/include)
+target_link_libraries(vma PUBLIC ${Vulkan_LIBRARIES})
+target_compile_options(vma PRIVATE -w) 
+   
+LinkTest(vma)
+
+
