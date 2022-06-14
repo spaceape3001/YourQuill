bddb4981955fccb887f949ecf1b867294cd66cb0
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-06-14 08:41:56 +0000

    And missed a file, pushing

--- api/engine/enum/engine_enums.cpp
+++ api/engine/enum/engine_enums.cpp
@@ -7,28 +7,44 @@
 #include <basic/meta/Init.hpp>
 #include "BorderColor.hpp"
 #include "ColorComponent.hpp"
+#include "ComponentSwizzle.hpp"
 #include "CullMode.hpp"
 #include "DataFormat.hpp"
 #include "DescriptorType.hpp"
 #include "FilterType.hpp"
 #include "FrontFace.hpp"
+#include "ImageLayout.hpp"
+#include "ImageTiling.hpp"
+#include "ImageType.hpp"
 #include "LogicOp.hpp"
 #include "MipmapMode.hpp"
 #include "PolygonMode.hpp"
+#include "SamplerAddressMode.hpp"
+#include "SamplerMipmapMode.hpp"
 #include "StencilOp.hpp"
+#include "SubpassContents.hpp"
+#include "SystemAllocationScope.hpp"
 #include "Topology.hpp"
 #include "VertexInputRate.hpp"
 
 YQ_TYPE_IMPLEMENT(yq::engine::BorderColor)
 YQ_TYPE_IMPLEMENT(yq::engine::ColorComponent)
 YQ_TYPE_IMPLEMENT(yq::engine::ColorComponentFlags)
+YQ_TYPE_IMPLEMENT(yq::engine::ComponentSwizzle)
 YQ_TYPE_IMPLEMENT(yq::engine::CullMode)
 YQ_TYPE_IMPLEMENT(yq::engine::DataFormat)
 YQ_TYPE_IMPLEMENT(yq::engine::DescriptorType)
 YQ_TYPE_IMPLEMENT(yq::engine::FilterType)
 YQ_TYPE_IMPLEMENT(yq::engine::FrontFace)
+YQ_TYPE_IMPLEMENT(yq::engine::ImageLayout)
+YQ_TYPE_IMPLEMENT(yq::engine::ImageTiling)
+YQ_TYPE_IMPLEMENT(yq::engine::ImageType)
 YQ_TYPE_IMPLEMENT(yq::engine::LogicOp)
 YQ_TYPE_IMPLEMENT(yq::engine::MipmapMode)
 YQ_TYPE_IMPLEMENT(yq::engine::PolygonMode)
+YQ_TYPE_IMPLEMENT(yq::engine::SamplerAddressMode)
+YQ_TYPE_IMPLEMENT(yq::engine::SamplerMipmapMode)
 YQ_TYPE_IMPLEMENT(yq::engine::StencilOp)
+YQ_TYPE_IMPLEMENT(yq::engine::SubpassContents)
+YQ_TYPE_IMPLEMENT(yq::engine::SystemAllocationScope)
 YQ_TYPE_IMPLEMENT(yq::engine::Topology)
