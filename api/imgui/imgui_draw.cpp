1d12e4f66a2485a0a320a91b1d247445b27ce9e0
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-07-03 09:06:03 +0000

    Targeted warning disabled due to compiler bug?
    Compiler seems to be reading in a negative size_t while if() prevent
    that, therefore has to be compiler bug?  Anyways, disabling it.

--- api/imgui/imgui_draw.cpp
+++ api/imgui/imgui_draw.cpp
@@ -87,6 +87,7 @@ Index of this file:
 #pragma GCC diagnostic ignored "-Wconversion"               // warning: conversion to 'xxxx' from 'xxxx' may alter its value
 #pragma GCC diagnostic ignored "-Wstack-protector"          // warning: stack protector not protecting local variables: variable length buffer
 #pragma GCC diagnostic ignored "-Wclass-memaccess"          // [__GNUC__ >= 8] warning: 'memset/memcpy' clearing/writing an object of type 'xxxx' with no trivial copy-assignment; use assignment or value-initialization instead
+#pragma GCC diagnostic ignored "-Wstringop-overflow"        // warning: memset exceeds maximum object size (compiler bug)
 #endif
 
 //-------------------------------------------------------------------------
