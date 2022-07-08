a04b31d5b9d83e23a4db0e141c1df2d56181698f
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-07-08 13:44:57 +0000

    Editor switch to widget

--- api/edit/Editor.cpp
+++ api/edit/Editor.cpp
@@ -5,19 +5,31 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "Editor.hpp"
+#include <imgui.h>
 
-YQ_OBJECT_IMPLEMENT(yq::Editor)
+YQ_OBJECT_IMPLEMENT(yq::edit::Editor)
 
 namespace yq {
-    EditorInfo::EditorInfo(std::string_view n, ObjectInfo& base, const std::source_location& sl) : ObjectInfo(n, base, sl)
-    {
-    }
+    namespace edit {
+        EditorInfo::EditorInfo(std::string_view n, engine::WidgetInfo& base, const std::source_location& sl) : WidgetInfo(n, base, sl)
+        {
+        }
 
-    Editor::Editor()
-    {
-    }
-    
-    Editor::~Editor()
-    {
+        Editor::Editor()
+        {
+        }
+        
+        Editor::~Editor()
+        {
+        }
+
+        void    Editor::draw(engine::UiData&ui) 
+        {
+            if(ImGui::BeginMainMenuBar()){
+                draw_menubar();
+                ImGui::EndMainMenuBar();
+            }
+            draw_content();
+        }
     }
 }
--- api/edit/Editor.hpp
+++ api/edit/Editor.hpp
@@ -6,36 +6,54 @@
 
 #pragma once
 
-#include <basic/Object.hpp>
-#include <basic/meta/ObjectInfo.hpp>
+#include <engine/Widget.hpp>
 #include <variant>
 #include <filesystem>
 
 namespace yq {
-    class EditorInfo : public ObjectInfo {
-    public:
+    namespace edit {
+        class EditorInfo : public engine::WidgetInfo {
+        public:
+            
+            template <typename C>  struct Writer;
+            
+            EditorInfo(std::string_view, engine::WidgetInfo&, const std::source_location& sl = std::source_location::current());
+        };
         
-        template <typename C>  struct Writer;
+        using EditorSpec    = std::variant<std::monostate, uint64_t, std::filesystem::path>;
         
-        EditorInfo(std::string_view, ObjectInfo&, const std::source_location& sl = std::source_location::current());
-    };
-    
-    using EditorSpec    = std::variant<std::monostate, uint64_t, std::filesystem::path>;
-    
-    class Editor : public Object {
-        YQ_OBJECT_INFO(EditorInfo)
-        YQ_OBJECT_DECLARE(Editor, Object)
-    public:
-    
-        virtual bool    load(EditorSpec) = 0;
-        virtual void    draw_imgui() {}
+        class Editor : public engine::Widget {
+            YQ_OBJECT_INFO(EditorInfo)
+            YQ_OBJECT_DECLARE(Editor, engine::Widget)
+        public:
         
-        const std::filesystem::path&    file() const { return m_file; }
-    protected:
+            virtual bool    load(EditorSpec) = 0;
+            
+            virtual void    draw_menubar(){}
+            virtual void    draw_content(){}
+            
+            virtual void    draw(engine::UiData&) override;
+            
+            const std::filesystem::path&    file() const { return m_file; }
+        protected:
 
-        std::filesystem::path   m_file;
-    
-        Editor();
-        ~Editor();
-    };
+            std::filesystem::path   m_file;
+        
+            Editor();
+            ~Editor();
+        };
+
+        template <typename C>
+        class EditorInfo::Writer : public engine::WidgetInfo::Writer<C> {
+        public:
+        
+            Writer(EditorInfo* editorInfo) : engine::WidgetInfo::Writer<C>(editorInfo)
+            {
+            }
+            
+            Writer(EditorInfo& editorInfo) : Writer(&editorInfo)
+            {
+            }
+        };
+    }
 }
