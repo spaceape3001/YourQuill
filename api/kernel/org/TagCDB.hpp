46614a01211a821efa598f89cbb03c1f69d70fd5
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-08-26 09:01:24 +0000

    Doxygen

--- api/kernel/org/TagCDB.hpp
+++ api/kernel/org/TagCDB.hpp
@@ -46,32 +46,77 @@ namespace yq {
         /*! \brief Creates/Gets a tag by document
         */
         Tag                     db_tag(Document, bool* wasCreated=nullptr);
+
+        /*! \brief Creates/Gets a tag by fragment
+        */
         Tag                     db_tag(Fragment, bool* wasCreated=nullptr);
+
+        /*! \brief Creates/Gets a tag by key
+        */
         Tag                     db_tag(std::string_view,  bool* wasCreated=nullptr);
+
+        /*! \brief Creates tags by keys
+        */
         std::vector<Tag>        db_tags(string_view_set_t);
+
+        /*! \brief Creates tags by keys
+        */
         std::vector<Tag>        db_tags(string_set_t);
 
+        /*! \brief Gets the tag's document
+        */
         Document                document(Tag);
 
+        /*! \brief Erases the tag
+        */
         void                    erase(Tag);
+        
+        /*! \brief Checks for tags existance
+        */
         bool                    exists(Tag);
+
+        /*! \brief Checks for tags existance
+        */
         bool                    exists_tag(uint64_t);
+        
+        /*! \brief Icon for tag
+        */
         Image                   icon(Tag);
         
+        /*! \brief Info for tag
+        */
         Tag::Info               info(Tag, bool autoKeyToName=false);
 
         //bool                    is_tagged(Field, Tag);
 
+        /*! \brief Key for tag
+        */
         std::string             key(Tag);
         
+        /*! \brief Label for tag
+        */
         std::string             label(Tag);
+        
+        /*! \brief Leaf for tag
+        */
         Leaf                    leaf(Tag t);
         
+        /*! \brief Makes a tag
+        */
         Tag                     make_tag(std::string_view, const Root* rt=nullptr, cdb_options_t opts=0, bool* wasCreated=nullptr);
+        
+        /*! \brief Merged data for tag
+        
+            Fuses the data from all roots and fragments, returns it.
+        */
         Tag::SharedData         merged(Tag, cdb_options_t opts=0);
         
+        /*! \brief Name for tag
+        */
         std::string             name(Tag);
         
+        /*! \brief Name, Key, and Icon for tag
+        */
         NKI                     nki(Tag, bool autoKeyToName=false);
     
         //! \brief Returns the FIRST tag fragment that qualifies
@@ -79,24 +124,52 @@ namespace yq {
         //!     Will not create a fragment, though (so can return NULL)
         Tag::SharedFile         read(Tag, const Root*, cdb_options_t opts=0);
         
+        /*! \brief All fragments for tag
+        */
         std::vector<TagFragDoc> reads(Tag, cdb_options_t opts=0);
+
+        /*! \brief Fragments for tag in root
+        */
         std::vector<TagFragDoc> reads(Tag, class Root*, cdb_options_t opts=0);
         
+        /*! \brief Sets the brief information for tag
+        */
         bool                    set_brief(Tag, std::string_view, class Root* rt=nullptr);
+
+        /*! \brief Sets the name  information for tag
+        */
         bool                    set_name(Tag, std::string_view, class Root* rt=nullptr);
+
+        /*! \brief Sets the notes information for tag
+        */
         bool                    set_notes(Tag, std::string_view, class Root* rt=nullptr);
         
+        /*! \brief Looks up tag by key
+        */
         Tag                     tag(std::string_view);
+        
+        /*! \brief Looks up tag by ID
+        */
         Tag                     tag(uint64_t);
+
+        /*! \brief Looks up tag by document
+        */
         Tag                     tag(Document, bool calc=false);
         
+        //! \brief Tag File (&Data) for fragment
         Tag::SharedFile         tag_doc(Fragment, cdb_options_t opts=0);
 
+        /*! \brief Tags for key-set */
         std::vector<Tag>        tags(const string_set_t&, bool noisy=false);
+        /*! \brief Tags for key-set */
         std::set<Tag>           tags_set(const string_set_t&, bool noisy=false);
+        /*! \brief Tags for key-set */
         std::set<Tag>           tags_set(const string_view_set_t&, bool noisy=false);
 
+        //! Updates Tag Icon
         void                    update_icon(Tag);
+        
+        //! Updates the tag data
         Tag::SharedData         update(Tag, cdb_options_t opts=0);
 
         //!  \brief   Returns a writable document
