33afe61c33c6832cb65ea860dcfda44eac4b3fca
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-08-29 08:50:42 +0000

    Doxygen

--- api/kernel/file/RootCDB.hpp
+++ api/kernel/file/RootCDB.hpp
@@ -18,35 +18,54 @@ namespace yq {
     
     namespace cdb {
 
+        /*! \brief All directories for root
+        */
         std::vector<Directory>     all_directories(const Root*, Sorted sorted=Sorted());
+        
+        //! \brief Count of directories under root
         size_t                     all_directories_count(const Root*);
 
+        //! \brief All fragments for root
         std::vector<Fragment>      all_fragments(const Root*,Sorted sorted=Sorted());
+        
+        //! \brief Count of all fragments for root.
         size_t                     all_fragments_count(const Root*);
         
-        size_t                      all_roots_count();
+        //! Number of roots
+        size_t                     all_roots_count();
 
+        //! Immediate children of root (no subs)
         std::vector<DirOrFrag>     children(const Root*,Sorted sorted=Sorted());
 
+        //! Create root directory
         Directory                  db_root(const Root*, bool *wasCreated=nullptr);
 
+        //! Top Directory for root
         Directory                  directory(const Root*);
-        Directory                  directory(const Root*, std::string_view);
+        //Directory                  directory(const Root*, std::string_view);
         
+        //! Top directories for root (should be one)
         std::vector<Directory>     directories(const Root*, Sorted sorted=Sorted());
         
+        //! Count of top directories for root (should be one)
         size_t                     directories_count(const Root*);
 
+        //! True if the relative file exists
         bool                       exists(const Root*, std::string_view );
 
-        bool                       exists_fragment(uint64_t);
-
+        //! Fragment for resolving root
         Fragment                   fragment(const Root*, std::string_view );
+        
+        //! Child fragments for root top directory
         std::vector<Fragment>      fragments(const Root*, Sorted sorted=Sorted());
 
+        //! Key of root (already on structure)
         std::string                key(const Root*);
 
+        //! Resolves the path, optionally creating the directories
         std::filesystem::path      path(const Root*, std::string_view , bool fMakePath=false);
+
+        //! Resolves the path, optionally creating the directories
         std::filesystem::path      path(const Root*, Document, bool fMakePath=false);          // goes to root
 
     }
