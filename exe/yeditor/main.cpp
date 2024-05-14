eaa0f40ae83f03de5eb2f1bbf3c6e9e7a9b5a320
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-05-14 09:55:49 +0000

    Resources
    Moved the Qt resources to a common location

--- exe/yeditor/main.cpp
+++ exe/yeditor/main.cpp
@@ -9,6 +9,7 @@
 #include <gluon/core/Utilities.hpp>
 #include <0/io/PluginLoader.hpp>
 #include <0/meta/Meta.hpp>
+#include <yqResources.hpp>
 
 using namespace yq;
 
@@ -21,6 +22,7 @@ int main(int argc, char* argv[])
     DreamApp    app(argc, argv);
     if(!app.configure())
         return -1;
+    yq::resources::load_standard();
 
     Meta::init();
     load_plugin_dir("plugin");
