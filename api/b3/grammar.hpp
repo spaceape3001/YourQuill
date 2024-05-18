181cbe6fa76256e7c8558ce311471f60f04bda67
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-05-18 15:54:08 +0000

    B3 Line Parser
    Starting up the old 3x map code, so starting with a parser.

---
+++ api/b3/grammar.hpp
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <b3/preamble.hpp>
+#include <0/basic/preamble.hpp>
+#include <0/basic/Flags.hpp>
+#include <utility>
+
+namespace yq::b3 {
+    struct B3Line {
+        arg_vector_t    tokens;                     //!< arguments 0, 1, 2...
+        uint32_t        line        = 0;            //!< Line number
+        uint16_t        indent      = 0;            //!< Line indent
+        
+        static Expect<B3Line>   decode(const char*& text, const char* last, uint32_t& line);
+        static Expect<B3Line>   decode(std::string_view);
+        
+        bool    empty() const { return tokens.empty(); }
+    };
+    
+    struct B3Error {
+        std::string     message;
+        uint32_t        line    = 0;
+    };
+    
+    //using B3Result  = 
+    
+    struct B3Command {
+        arg_vector_t    pArgs;                      //!< Positional arguments 0, 1, 2...
+        arg_map_t       nArgs;                      //!< Named arguments x=foo
+        b3cmd_vector_t  subcommands;
+        uint32_t        line        = 0;            //!< Line number
+        uint16_t        indent      = 0;            //!< Line indent
+    };
+
+    struct B3Document {
+        std::filesystem::path       file;
+        b3cmd_vector_t              commands;
+        
+        static std::pair<B3Document,std::error_code>    load(const std::filesystem::path&);
+        static std::pair<B3Document,std::error_code>    load(std::string_view);
+    };
+
+}
